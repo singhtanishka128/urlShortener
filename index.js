39f0cf65bb33419046aebca83e0c21b64a72a43b
Tanishka Singh <singhtanishka128@gmail.com>
2025-11-16 20:50:40 +0530

    ssr using ejs

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const urlRoute = require("./routes/url.js")
 const { connectToMongoDB } = require("./connect")
 const URL = require("./models/url")
+const staticRoute = require("./routes/staticRouter.js")
 require('dotenv').config();
+const path = require("path")
 
 
 const app = express()
@@ -10,9 +12,24 @@ const PORT = 8000
 
 connectToMongoDB(process.env.MONGODB_URL).then(()=>console.log("mongodb connected"))
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"))
+
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.get("/test", async (req,res)=>{
+    const allUrls = await URL.find({});
+    return res.render("home", {
+        urls: allUrls,
+    })
+})
+
 app.use("/url", urlRoute)
-app.get("/:shortId", async (req,res)=>{
+app.use("/",staticRoute)
+
+app.get("/url/:shortId", async (req,res)=>{
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate(
         {
